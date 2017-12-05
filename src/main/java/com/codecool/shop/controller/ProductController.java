a60549ff1d16e480e9aa3c9f2308594aaadc4fc9
György Mátyás <gy.matyas97@gmail.com>
2017-12-05 17:00:40 +0100

    removed redundancy

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -2,9 +2,7 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.Utils;
 import com.codecool.shop.dao.ProductCategoryDao;
-import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
-import com.codecool.shop.dao.implementation.ProductDaoMem;
 import spark.Request;
 import spark.Response;
 
@@ -14,7 +12,6 @@ import java.util.Map;
 public class ProductController {
 
     public static String renderProducts(Request req, Response res) {
-        ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
         Utils utils = Utils.getInstance();
 
