2013cf6fbdf8239da39b7622cb48ad227184888e
gabor-szeles <szelesgbr@gmail.com>
2017-12-05 17:15:23 +0100

    added methods to ORder class

--- src/main/java/com/codecool/shop/model/Order.java
+++ src/main/java/com/codecool/shop/model/Order.java
@@ -4,12 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
-    List<LineItem> addedItems;
-    int totalPrice;
 
-    Order() {
+    List<LineItem> addedItems;
+    float totalPrice;
+    static Order currentOrder;
+
+    public Order() {
         this.addedItems = new ArrayList<>();
         totalPrice = 0;
+        this.currentOrder = this;
+    }
+
+    public static Order getCurrentOrder() {
+        return currentOrder;
+    }
+
+    public void add (LineItem lineItem) {
+        this.addedItems.add(lineItem);
+        this.totalPrice += lineItem.getItemPriceSum();
+    }
+
+    public List<LineItem> getAddedItems() {
+        return addedItems;
     }
 
 }
