5b8aeeb218b6a71b1688e05c4114c98f771f3f60
gabor-szeles <szelesgbr@gmail.com>
2017-12-05 17:00:22 +0100

    scripts folder added

---
+++ src/main/resources/public/script/dom.js
@@ -0,0 +1,22 @@
+const dom = {
+
+    addEventListeners: function () {
+        $('.btn-success').click(function(event) {
+            var clickedButton = event.target;
+            var productId = {"productid" : clickedButton.dataset.prod_id};
+            $.ajax({
+                type: "POST",
+                url: "/api/additem",  // Send the login info to this page
+                data: JSON.stringify(productId),
+                dataType: "json",
+                contentType: "application/json",
+                success: function (response) {
+                    console.log("Success")
+                }
+            });
+        })
+    }
+
+}
+
+dom.addEventListeners();
