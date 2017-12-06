6ca48401efd20ce2753380b9c31537a2c703c54f
gabor-szeles <szelesgbr@gmail.com>
2017-12-06 16:08:49 +0100

    js logics for review cart added

--- src/main/resources/public/script/dom.js
+++ src/main/resources/public/script/dom.js
@@ -1,19 +1,10 @@
 const dom = {
 
     addEventListeners: function () {
-        $('.btn-success').click(function(event) {
-            var clickedButton = event.target;
-            var productId = {"productid" : clickedButton.dataset.prod_id};
-            $.ajax({
-                type: "POST",
-                url: "/api/additem",  // Send the login info to this page
-                data: JSON.stringify(productId),
-                dataType: "json",
-                contentType: "application/json",
-                success: dom.refreshHeader,
+        $('.addtocart').click(dom.addToCartClick);
+        $('#shoppingCart').on('show.bs.modal', dom.reviewCart);
+        $('#shoppingCart').on('hide.bs.modal', dom.deleteModalContent);
 
-            });
-        })
     },
 
     refreshHeader: function (response) {
@@ -22,6 +13,47 @@ const dom = {
         $('#total-items').text(itemsNumber);
         $('#total-price').text(totalPrice);
 
+    },
+
+    addToCartClick: function (event) {
+        var clickedButton = event.target;
+        var productId = {"productid" : clickedButton.dataset.prod_id};
+        $.ajax({
+            type: "POST",
+            url: "/api/additem",  // Send the login info to this page
+            data: JSON.stringify(productId),
+            dataType: "json",
+            contentType: "application/json",
+            success: dom.refreshHeader,
+        });
+
+    },
+
+    reviewCart: function () {
+        $.ajax({
+            type: "POST",
+            url: "/review",
+            success: dom.drawReview,
+        });
+    },
+
+    drawReview: function (response) {
+        var list = JSON.parse(response).shoppingCart;
+        for (let i = 0; i<list.length;i++) {
+            console.log("INSIDE")
+            $('#reviewTable').append(`
+                <tr class="addedRow">
+                    <td>${list[i].name}</td>
+                    <td>${list[i].quantity}</td>
+                    <td>${list[i].price}</td>
+                </tr>
+            `);
+
+        }
+    },
+
+    deleteModalContent: function () {
+        $('.addedRow').empty();
     }
 
 }
