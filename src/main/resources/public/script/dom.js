c2166b7f73c6a1f49f12a610403480bf9a45dd7b
Bence Lovas <bansoul1@gmail.com>
2017-12-06 19:54:21 +0100

    refactor dom.js

--- src/main/resources/public/script/dom.js
+++ src/main/resources/public/script/dom.js
@@ -1,91 +1,52 @@
 $(document).ready(function() {
 
     const dom = {
+
+        // EVENTS
+
         addToCartClick: function (event) {
-            var clickedButton = event.target;
-            var productId = {"productid" : clickedButton.dataset.prod_id};
+            let clickedButton = event.target;
+            let productId = {"productid" : clickedButton.dataset.prod_id};
             $.ajax({
                 type: "POST",
                 url: "/api/additem",  // Send the login info to this page
                 data: JSON.stringify(productId),
                 dataType: "json",
                 contentType: "application/json",
-                success: dom.refreshHeader,
+                success: dom.refreshCart,
             });
         },
         reviewCart: function () {
             $("#cart").slideToggle();
         },
-        drawReview: function (response) {
-            var list = response.shoppingCart;
-            let cart = $("#cart");
-            for (let i = 0; i<list.length;i++) {
-                let wrapper = $('<div/>', {
-                    "class": "row"
-                });
-                let name = $('<p/>', {}).text(list[i].name);
-                let quantity = $('<p/>', {}).text(list[i].quantity);
-                let price = $('<p/>', {}).text(list[i].price);
-                wrapper
-                    .append(name)
-                    .append(quantity)
-                    .append(price);
-                cart.hide();
-                cart.append(wrapper).slideToggle();
-                cart.click(function(event) {
-                    $(event.target).slideToggle();
-                })
-            }
-        },
         supplierButtonClickHandler: function(event) {
             let id = $(event.target).attr("id");
-
             id = id.replace('supplier', '');
-            console.log("id: " + id);
-            let data = {"id": id};
             $.ajax({
                 type: "GET",
                 url: "/api/get-supplier-products/" + id,
                 dataType: "json",
                 contentType: "application/json",
                 success: function(response) {
-                    console.log(response);
                     $("#collectionName").text(response.collectionName);
                     let productDiv = $("#products");
                     productDiv.empty();
-                    console.log(response.collection);
                     for (let i = 0; response.collection.length; i++) {
-                        let outerWrapper = $('<div/>', {
-                            "class": "item col-xs-4 col-lg-4",
-                        });
-                        let wrapper = $('<div/>', {
-                            "class": "thumbnail",
-                        });
+                        let outerWrapper = $('<div/>', {"class": "item col-xs-4 col-lg-4"});
+                        let wrapper = $('<div/>', {"class": "thumbnail"});
                         let image = $('<img/>', {
                             "class": "group list-group-image",
                             src: "http://placehold.it/400x250/000/fff",
                         });
-                        let innerWrapper = $('<div/>', {
-                            "class": "caption",
-                        });
-                        let productName = $('<h4/>', {
-                            "class": "group inner list-group-item-heading"
-                        }).text(response.collection[i].name);
-                        let productDescription = $('<p/>', {
-                            "class": "group inner list-group-item-text",
-                        }).text(response.collection[i].description);
-                        let row = $('<div/>', {
-                            "class": "row"
-                        });
-                        let priceWrapper = $('<div/>', {
-                            "class": "col-xs-12 col-md-6"
-                        });
-                        let price = $('<p/>', {
-                            "class": "lead"
-                        }).text(response.collection[i].price);
-                        let addToCartWrapper = $('<div/>', {
-                            "class": "col-xs-12 col-md-6",
-                        });
+                        let innerWrapper = $('<div/>', {"class": "caption"});
+                        let productName = $('<h4/>', {"class": "group inner list-group-item-heading"})
+                            .text(response.collection[i].name);
+                        let productDescription = $('<p/>', {"class": "group inner list-group-item-text"})
+                            .text(response.collection[i].description);
+                        let row = $('<div/>', {"class": "row"});
+                        let priceWrapper = $('<div/>', {"class": "col-xs-12 col-md-6"});
+                        let price = $('<p/>', {"class": "lead"}).text(response.collection[i].price);
+                        let addToCartWrapper = $('<div/>', {"class": "col-xs-12 col-md-6"});
                         let addToCart = $('<a/>', {
                             "class": "btn btn-success addtocart",
                             "data-prod_id": response.collection[i].id,
@@ -100,44 +61,37 @@ $(document).ready(function() {
                         wrapper.append(innerWrapper);
                         outerWrapper.append(wrapper);
                         productDiv.append(outerWrapper);
-                        console.log("lul");
                     }
                     dom.addEventListenersToAddToCart();
                 }
             });
         },
-        addEventsToSupplierButtons: function() {
-            let buttons = $("button[id*='supplier']");
-            buttons.click(dom.supplierButtonClickHandler);
-        },
-        refreshHeader: function (response) {
-            console.log(response);
-            var itemsNumber = response.itemsNumber;
-            var totalPrice = response.totalPrice;
+        refreshCart: function (response) {
+            let itemsNumber = response.itemsNumber;
+            let totalPrice = response.totalPrice;
             $('#total-items').text(itemsNumber);
             $('#total-price').text(totalPrice);
-            var list = response.shoppingCart;
-            console.log("list " + list);
+            let products = response.shoppingCart;
             let cart = $("#cart");
             cart.empty();
-            for (let i = 0; i<list.length;i++) {
-                let wrapper = $('<div/>', {
-                    "class": "row"
-                });
-                let name = $('<p/>', {}).text(list[i].name);
-                let quantity = $('<p/>', {}).text(list[i].quantity);
-                let price = $('<p/>', {}).text(list[i].price);
+            for (let i = 0; i<products.length;i++) {
+                let wrapper = $('<div/>', {"class": "row"});
+                let name = $('<p/>', {}).text(products[i].name);
+                let quantity = $('<p/>', {}).text(products[i].quantity);
+                let price = $('<p/>', {}).text(products[i].price);
                 wrapper
                     .append(name)
                     .append(quantity)
-                    .append(price);
-                cart.append(wrapper);
-                // cart.hide();
-                // cart.append(wrapper).slideToggle();
-                // cart.click(function(event) {
-                //     $(event.target).slideToggle();
-                // })
+                    .append(price)
+                    .appendTo(cart);
             }
+        },
+
+        // EVENT APPLIERS
+
+        addEventsToSupplierButtons: function() {
+            let buttons = $("button[id*='supplier']");
+            buttons.click(dom.supplierButtonClickHandler);
         },
         addEventListenersToHeader: function () {
             $('#cartButton').click(dom.reviewCart);
@@ -145,7 +99,9 @@ $(document).ready(function() {
         addEventListenersToAddToCart: function() {
             $('.addtocart').click(dom.addToCartClick);
         },
+    };
 
+    const ajax = {
 
     };
 
