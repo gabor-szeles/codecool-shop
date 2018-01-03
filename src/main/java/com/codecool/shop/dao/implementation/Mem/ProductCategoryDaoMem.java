06e9e094f7c0861baa47577cb5d7e98d4b612919
György Mátyás <gy.matyas97@gmail.com>
2018-01-03 14:42:00 +0100

    clear DaoMem DATA methods

--- src/main/java/com/codecool/shop/dao/implementation/Mem/ProductCategoryDaoMem.java
+++ src/main/java/com/codecool/shop/dao/implementation/Mem/ProductCategoryDaoMem.java
@@ -44,4 +44,8 @@ public class ProductCategoryDaoMem implements ProductCategoryDao {
     public List<ProductCategory> getAll() {
         return DATA;
     }
+
+    public void clear() {
+        DATA.clear();
+    }
 }
