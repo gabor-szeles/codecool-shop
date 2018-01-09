a2f6af16a1dba5f51eb3099f06c11a60487aa3fe
gabor-szeles <szelesgbr@gmail.com>
2018-01-09 16:47:22 +0100

    added documentation for UserDao interface

--- src/main/java/com/codecool/shop/dao/UserDao.java
+++ src/main/java/com/codecool/shop/dao/UserDao.java
@@ -3,8 +3,22 @@ package com.codecool.shop.dao;
 
 import com.codecool.shop.model.User;
 
+/**
+ * UserDao is the interface providing access to User data in the database.
+ */
 public interface UserDao {
+
+    /**
+     * Adds a User record to the database
+     * @param user User object to be added
+     */
     boolean add(User user);
 
+    /**
+     * Finds a user in the database by its user name
+     * @param username The user name of the user to be found
+     * @return Returns the User object found
+     * @throws java.sql.SQLException when the user cannot be found in the database
+     */
     User find(String username);
 }
