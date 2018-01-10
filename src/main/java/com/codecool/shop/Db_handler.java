66492b16cbd4712a64b420371f7174af4ae80942
György Mátyás <gy.matyas97@gmail.com>
2018-01-10 13:52:37 +0100

    javadoc for Db_handler

--- src/main/java/com/codecool/shop/Db_handler.java
+++ src/main/java/com/codecool/shop/Db_handler.java
@@ -11,14 +11,27 @@ import java.io.IOException;
 import java.sql.*;
 import java.util.Properties;
 
+
+/**
+ * Db_handler is the class providing connection to postgreSQL database.
+ */
 public class Db_handler {
 
+    /**
+     * Db handler instance to be returned every time.
+     */
     private static Db_handler instance = null;
 
-
+    /**
+     * A private Constructor prevents any other class from instantiating.
+     */
     private Db_handler() {
     }
 
+    /**
+     * Returns the database handler object
+     * @return Db_handler object instance.
+     */
     public static Db_handler getInstance() {
         if (instance == null) {
             instance = new Db_handler();
@@ -26,10 +39,14 @@ public class Db_handler {
         return instance;
     }
 
-
+    /**
+     * Establishes a connection with postgresSQL database.
+     * @throws SQLException if connection cannot be established.
+     * @return Returns Connection object to the DB.
+     */
     public Connection getConnection() throws SQLException {
         Properties props = new Properties();
-        FileInputStream in = null;
+        FileInputStream in;
         try {
             in = new FileInputStream("configuration/DataBase.properties");
             try {
@@ -49,6 +66,11 @@ public class Db_handler {
         return DriverManager.getConnection(url, username, password);
     }
 
+    /**
+     * Executes a PreparedStatement when there is a ResultSet object to return.
+     * @param preparedStatement PreparedStatement object to be executed.
+     * @return ResultSet object as a cursor in DB.
+     */
     private ResultSet executePreparedStatementQuery(PreparedStatement preparedStatement) {
         try {
             return preparedStatement.executeQuery();
@@ -58,6 +80,10 @@ public class Db_handler {
         return null;
     }
 
+    /**
+     * Executes a PreparedStatement when there is no ResultSet object to return.
+     * @param preparedStatement PreparedStatement object to be executed.
+     */
     private void executePreparedStatement(PreparedStatement preparedStatement) {
         try {
             preparedStatement.execute();
@@ -66,6 +92,11 @@ public class Db_handler {
         }
     }
 
+    /**
+     * Creates and executes a PreparedStatement for adding BaseModel object to DB.
+     * @param object BaseModel object which needs to be added to DB.
+     * @param query Query to be rendered as PreparedStatement.
+     */
     public void createPreparedStatementForAdd(BaseModel object, String query) {
         try {
             Connection conn = getConnection();
@@ -87,6 +118,12 @@ public class Db_handler {
         }
     }
 
+    /**
+     * Fills the PreparedStatementFields's for adding ProductCategory to DB.
+     * @param productCategory ProductCategory object which needs to be added to DB.
+     * @param prepStatement PreparedStatement object to be filled with data from productCategory object.
+     * @throws SQLException if the number of fields in PreparedStatement doesn't match the number of arguments to be filled.
+     */
     private void fillPreparedStatementFields(ProductCategory productCategory, PreparedStatement prepStatement) throws SQLException {
         prepStatement.setInt(1, productCategory.getId());
         prepStatement.setString(2, productCategory.getName());
@@ -94,12 +131,24 @@ public class Db_handler {
         prepStatement.setString(4, productCategory.getDepartment());
     }
 
+    /**
+     * Fills the PreparedStatement's fields for adding Supplier to DB.
+     * @param supplier Supplier object which needs to be added to DB.
+     * @param prepStatement PreparedStatement object to be filled with data from supplier object.
+     @throws SQLException if the number of fields in PreparedStatement doesn't match the number of arguments to be filled.
+     */
     private void fillPreparedStatementFields(Supplier supplier, PreparedStatement prepStatement) throws SQLException {
         prepStatement.setInt(1, supplier.getId());
         prepStatement.setString(2, supplier.getName());
         prepStatement.setString(3, supplier.getDescription());
     }
 
+    /**
+     * Fills the PreparedStatement's fields for adding Product to DB.
+     * @param product Product object which needs to be added to DB.
+     * @param prepStatement PreparedStatement object to be filled with data from Product object.
+     @throws SQLException if the number of fields in PreparedStatement doesn't match the number of arguments to be filled.
+     */
     private void fillPreparedStatementFields(Product product, PreparedStatement prepStatement) throws SQLException {
         prepStatement.setInt(1, product.getId());
         prepStatement.setString(2, product.getName());
@@ -110,6 +159,12 @@ public class Db_handler {
         prepStatement.setInt(7, product.getSupplier().getId());
     }
 
+    /**
+     * Creates and executes a PreparedStatement for finding record in DB.
+     * @param id ID of the wanted element.
+     * @param query Query to be rendered as PreparedStatement.
+     * @return ResultSet object with pointer to found element.
+     */
     public ResultSet createPreparedStatementForFind(int id, String query) {
         try {
             Connection conn = getConnection();
@@ -122,6 +177,11 @@ public class Db_handler {
         return null;
     }
 
+    /**
+     * Creates and executes a PreparedStatement for removing record from DB.
+     * @param id ID of the wanted element.
+     * @param query Query to be rendered as PreparedStatement.
+     */
     public void createPreparedStatementForRemove(int id, String query) {
         try {
             Connection conn = getConnection();
@@ -133,6 +193,11 @@ public class Db_handler {
         }
     }
 
+    /**
+     * Creates and executes a PreparedStatement for getting all records in from selected DB table.
+     * @param query Query to be rendered as PreparedStatement.
+     * @return ResultSet object with pointer to found elements.
+     */
     public ResultSet createPreparedStatementForGetAll(String query) {
         try {
             Connection conn = getConnection();
