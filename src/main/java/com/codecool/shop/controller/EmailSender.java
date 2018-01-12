4898cf3e7e63aab9b6e40562799b70bdaf4d7428
Bence Lovas <bansoul1@gmail.com>
2018-01-12 17:21:12 +0100

    javadoc

--- src/main/java/com/codecool/shop/controller/EmailSender.java
+++ src/main/java/com/codecool/shop/controller/EmailSender.java
@@ -9,18 +9,37 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
+/**
+ * Sends welcome email to a specific user address.
+ * <p>
+ * The recipient's address is specified in the constructor and sent through the <strong>gmail</strong> address specified in the fields.
+ *
+ */
 public class EmailSender {
 
     private static String USER_NAME = "lavawebshop";
     private static String PASSWORD = "lavawebshop1";
     private String recipient;
 
+    /**
+     * Private constructor to prevent unparameterized instantiation.
+     */
     private EmailSender() {}
 
+    /**
+     * Create an instance of EmailSender.
+     *
+     * @param recipient The email address of the recipient.
+     */
     public EmailSender(String recipient) {
         this.recipient = recipient;
     }
 
+    /**
+     * Sends the email with the specified subject and message.
+     *
+     * Uses {@link #sendFromGMail(String, String, String[], String, String)} to send email.
+     */
     public void send() {
         String from = USER_NAME;
         String pass = PASSWORD;
@@ -31,6 +50,15 @@ public class EmailSender {
         sendFromGMail(from, pass, to, subject, body);
     }
 
+    /**
+     * Establishes the connection to the given <strong>gmail</strong> account and sends the email to the given addresses.
+     *
+     * @param from    The gmail account's address <strong>without @gmail.com</strong>, which the email will sent through.
+     * @param pass    The password for that gmail account.
+     * @param to      The address of the recipient.
+     * @param subject The subject of the email.
+     * @param body    The inner text of the email.
+     */
     private void sendFromGMail(String from, String pass, String[] to, String subject, String body) {
         Properties props = System.getProperties();
         String host = "smtp.gmail.com";
