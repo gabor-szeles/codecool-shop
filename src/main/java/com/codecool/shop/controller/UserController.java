beca96f933575e2ef03b7a4afae8297ee38425b8
Gergely Papi <gergely.papi.cc@gmail.com>
2018-01-14 19:12:22 +0100

    Added javadox to UserController.java

--- src/main/java/com/codecool/shop/controller/UserController.java
+++ src/main/java/com/codecool/shop/controller/UserController.java
@@ -11,11 +11,21 @@ import spark.Response;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Usercontroller is a class responsible to handle user session and verification.
+ */
 public class UserController {
 
     private static UserDao userDaoDb = UserDaoJdbc.getInstance();
     private static Utils utils = Utils.getInstance();
 
+    /**
+     * This method checks if a user is already logged in, if not redirects to the login page, otherwise establishes
+     * a session.
+     * @param req request sent from client side
+     * @param res response sent after processing the request
+     * @return empty string required for thymeleaf
+     */
     public static String renderLogin(Request req, Response res) {
         Map model = new HashMap();
 
@@ -28,6 +38,13 @@ public class UserController {
         return utils.renderTemplate(model, "product/login");
     }
 
+    /**
+     * This method is responsible for registering the users data based on request data. Also updates the database
+     * with the new data, including password encryption and confirmation e-mail sending.
+     * @param req request sent from client side
+     * @param res response sent after processing the request
+     * @return empty string required for thymeleaf
+     */
     public static String registration(Request req, Response res) {
         String saltedPassword = BCrypt.hashpw(req.queryParams("password"), BCrypt.gensalt());
         User user = new User(req.queryParams("name"), req.queryParams("email"), saltedPassword);
@@ -48,6 +65,13 @@ public class UserController {
         return "";
     }
 
+    /**
+     * This method is responsible for verifying existing user based on request data, and if the input machs the
+     * system lets the user in waiting for further instructions.
+     * @param req request sent from client side
+     * @param res response sent after processing the request
+     * @return empty string required for thymeleaf
+     */
     public static String login(Request req, Response res) {
         User user = new User(req.queryParams("name"), req.queryParams("password"));
         User selectedUser = userDaoDb.find(req.queryParams("name"));
@@ -70,6 +94,12 @@ public class UserController {
         return "";
     }
 
+    /**
+     * This method responsible for ceasing the session and redirecting to the login page requesting for a new session.
+     * @param req request sent from client side
+     * @param res response sent after processing the request
+     * @return empty string required for thymeleaf
+     */
     public static String logOut(Request req, Response res) {
         req.session().removeAttribute("username");
         res.redirect("/login");
@@ -77,6 +107,11 @@ public class UserController {
         return "";
     }
 
+    /**
+     * A check method that returns to the login page if no user is logged in.
+     * @param req request sent from client side
+     * @param res response sent after processing the request
+     */
     public static void ensureUserIsLoggedIn(Request req, Response res) {
         if (req.session().attribute("username") == null) {
             res.redirect("/login");
